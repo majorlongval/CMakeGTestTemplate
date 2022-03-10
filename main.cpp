e8d0f72bdc386c672e08203f36bbc3255cfa6122
JORDAN LONGVAL <majorlongval@gmail.com>
2022-03-09 21:18:49 -0500

    added a library and linked it to the main target

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "mylibfuncs.h"
+
 int main()
 {
     std::cout<<"Hello World!"<<std::endl;
---
+++ my_lib/mylibfuncs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "mylibfuncs.h"
+
+
+void sayHello(std::string name) 
+{
+    std::cout << "Hi Jordan!"<< std::endl;
+}
